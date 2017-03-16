a54e234e31e07b69170f83874fd1c841e58b4abe
XiaoJie Guo <guoxiaojie@onchain.com>
2017-03-16 16:40:52 +0800

    Test3
    Test3
    
    Signed-off-by: XiaoJie Guo <guoxiaojie@onchain.com>

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import "github.com/qingche123/SlackTest/test"
 
 func main(){
 	test.Print("123")
-	testPrint("123")	
-	testPrint("333123")
-	testPrint("333123111111111")
+	test.Print("123")	
+	test.Print("333123")
+	test.Print("333123111111111")
 	fmt.Println("Hello World\n")
 	fmt.Println("Hello World\n")	
 }

7868fa075b8017e203460ffd4dfe56b85941390f
ggxxjj123 <562783729@qq.com>
2017-03-16 16:18:30 +0800

    Test6
    Test6

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import "github.com/qingche123/SlackTest/test"
 func main(){
 	test.Print("123")
 	test.Print("321")
+	test.Print("4312")
 	fmt.Println("Hello World\n")
 	fmt.Println("Hello World\n")	
 }
